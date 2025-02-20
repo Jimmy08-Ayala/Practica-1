1b72f00a1bf2676b9d6be3fba67de68371f9486e
Jimmy08-Ayala <jimmy08.maurayala@gmail.com>
2025-02-20 11:29:43 -0600

    Add files via upload

---
+++ Practica 6/Evaluacion1_Jimmy/Evaluacion1Jimmy/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        int q;
+        double qcost;
+        double f;
+        double fCost;
+        double cantidad;
+        double cantidadC;
+        double m;
+        double mCost;
+        double totalPupusas;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             f= Convert.ToDouble(textBox2.Text);
+            q= Convert.ToInt32(textBox1.Text);
+            cantidad = Convert.ToDouble(textBox4.Text);
+            m= Convert.ToDouble(textBox5.Text);
+            fCost = f * 0.25;
+            qcost = q * 0.75;
+            cantidadC = cantidad * 0.70;
+            mCost = m * 0.65;
+            totalPupusas = fCost + qcost + cantidadC + mCost;
+            textBox3.Text = "$" + Convert.ToInt32(totalPupusas);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ Practica 6/Evaluación2_Jimmy/Evaluación2_Jimmy/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluación2_Jimmy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Visible = false;
+            textBox8.Visible = false;
+        }
+        double T1, T2, T3, L1, L2, L3;
+        double Promedio;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            T1 = Convert.ToDouble(textBox1.Text);
+            T2 = Convert.ToDouble(textBox2.Text);
+            T3 = Convert.ToDouble(textBox3.Text);
+            L1 = Convert.ToDouble(textBox4.Text);
+            L2 = Convert.ToDouble(textBox5.Text);
+            L3 = Convert.ToDouble(textBox6.Text);
+            Promedio = (T1 + T2 + T3 + L1 + L2 + L3)/ 6;
+
+            textBox7.Text = "" + Promedio;
+
+            if (Promedio >=7)
+            {
+                pictureBox1.Visible = true;
+                textBox8.Visible = true;
+                pictureBox1.BackgroundImage = Image.FromFile("1.jpg");
+                textBox8.Text = "PERFECTO! Sigue así >:D!";
+            }
+        }
+
+        
+    }
+}
